f352d075d48bbd30a566190835369b4cd982c9c3
Roland Holban <holbanroland@gmail.com>
2018-03-27 10:20:43 -0500

    Replaced autoincrement with faster alternative

--- database/schema.py
+++ database/schema.py
@@ -21,7 +21,7 @@ You can then use a_keyword.page to get to the page for that keyword'''
 
 
 class WebsiteVisits(Base.Model):
-    visitID = Base.Column(Base.Integer, primary_key=True, autoincrement=True)
+    visitID = Base.Column(Base.Integer, primary_key=True)
     url = Base.Column(Base.String(100), Base.ForeignKey('page.url'))
     keywords = Base.Column(Base.Text)
     activeRatio = Base.Column(Base.Float)
